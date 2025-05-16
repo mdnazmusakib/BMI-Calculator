781249e15ff6ba6eb674d61fb661138303998476
mdnazmusakib <md.nazmusakib02@gmail.com>
2025-05-17 00:07:12 +0600

    bmi calculation is done when btn is clicked working fine

--- bmi.js
+++ bmi.js
@@ -17,3 +17,32 @@ heightUnit.addEventListener('change', () => {
     ftInputDiv.classList.remove('hidden');
   }
 });
+
+calcBtn.addEventListener('click', () => {
+  let heightMeters;
+  if (heightUnit.value === 'cm') {
+    const cm = parseFloat(heightCm.value);
+    if (isNaN(cm) || cm <= 0) {
+      showError("Please enter a valid height in cm.");
+      return;
+    }
+    heightMeters = cm / 100;
+  } 
+  else {
+    const feet = parseFloat(heightFt.value);
+    const inches = parseFloat(heightIn.value);
+    if (isNaN(feet) || isNaN(inches) || feet < 0 || inches < 0) {
+      showError("Please enter valid feet and inches.");
+      return;
+    }
+    const totalInches = (feet * 12) + inches;
+    heightMeters = totalInches * 0.0254;
+  }
+  const weight = parseFloat(weightInput.value);
+  if (isNaN(weight) || weight <= 0) {
+    showError("Please enter a valid weight.");
+    return;
+  }
+  const bmi = (weight / (heightMeters * heightMeters)).toFixed(2);
+//   console.log(bmi);
+});
